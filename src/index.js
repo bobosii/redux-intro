9d8e600a17131ff34532c2db99084ba3b397a6b4
Emirhan DAY <emirhanday@Emirhan-Air.home>
2023-09-20 20:16:15 +0300

    createStore problem fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import configureStore from "./redux/reduces/configureStore";
+import configureStore from "./redux/reduces/configureStores";
 import { Provider } from "react-redux";
 
 const store = configureStore();
--- src/redux/reduces/index.js
+++ src/redux/reduces/index.js
@@ -5,4 +5,4 @@ const reducers = combineReducers({
   counterReducer,
 });
 
-export default reducers
+export default reducers;

7f07c7de9223c328598f1061eece767741ba5410
Emirhan DAY <emirhanday@Emirhan-Air.home>
2023-09-20 22:29:30 +0300

    last commit, project completed

--- src/components/DecreaseCounter.js
+++ src/components/DecreaseCounter.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { decreaseCounter } from "../redux/actions/counterActions";
+import { connect } from "react-redux";
 
-export default class DecreaseCounter extends Component {
+
+class DecreaseCounter extends Component {
   render() {
     return (
       <div>
-        
+        <button
+          onClick={(e) => {
+            this.props.dispatch(decreaseCounter());
+          }}
+        >
+          1 eksilt
+        </button>
       </div>
-    )
+    );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(decreaseCounter, dispatch) };
+}
+
+export default connect(mapDispatchToProps)(DecreaseCounter);
--- src/components/IncreaseByTwoCounter.js
+++ src/components/IncreaseByTwoCounter.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from "redux";
+import { increaseByTwoCounter } from "../redux/actions/counterActions";
+import { connect } from "react-redux";
 
-export default class IncreaseByTwoCounter extends Component {
+
+class IncreaseByTwoCounter extends Component {
   render() {
     return (
       <div>
-        
+        <button
+          onClick={(e) => {
+            this.props.dispatch(increaseByTwoCounter());
+          }}
+        >
+          2 arttır
+        </button>
       </div>
     )
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(increaseByTwoCounter, dispatch) };
+}
+
+export default connect(mapDispatchToProps)(IncreaseByTwoCounter);
